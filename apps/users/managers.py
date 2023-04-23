c3822310a22c89779d60dbf5572b7424e3e7fb3a
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-04-24 04:23:38 +0500

    feat: base manager

---
+++ apps/users/managers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("User must have an email!")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_super_user(self, email, password):
+        if not password:
+            raise ValueError("User must have a password!")
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from apps.common.models import BaseModel
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin, BaseModel):
-    pass
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+
+
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
