af12ef41ebad5d8cc59207c77f50dde7e477101e
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-05-02 23:50:38 +0500

    first commit

--- apps/social_auth/api_endpoints/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.social_auth.api_endpoints.views import GoogleLogin
-
-urlpatterns = [
-    path('google/', GoogleLogin.as_view(), name='google-social-login')
-]
