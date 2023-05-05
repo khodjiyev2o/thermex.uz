397207499389ca0ca253fad6149e456a9d23e18f
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-05-05 18:21:01 +0500

    fix: .env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 source venv/bin/activate
-pip install -r requirements/prod.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+pip3 install -r requirements/prod.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
+
