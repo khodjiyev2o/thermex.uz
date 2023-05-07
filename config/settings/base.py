c26bad0ac71ea7d009dda7a0f5f53a74e61c4b45
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-05-07 22:52:24 +0500

    feaT: static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,11 +101,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'staticfiles/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
