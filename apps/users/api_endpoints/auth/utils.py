8769c8023a4151b695734515266c6d1083e70a9f
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-05-25 00:53:32 +0500

    feat: env file

--- apps/users/api_endpoints/auth/utils.py
+++ apps/users/api_endpoints/auth/utils.py
@@ -2,13 +2,15 @@ import random
 import string
 from eskiz_sms import EskizSMS
 import sys
+import environ
+env = environ.Env()
 
 
 def send_activation_code_via_sms(phone: str, code: str):
     if "test" not in sys.argv:
         message_data = f"Thermex.uz uchun <#> Tasdiqlash kodi: {code}"
-        email = "samandarkhodjiyev@gmail.com"
-        password = "b9LHEGCG9fppE4B2D7rEexqk4AgYVMIUr10JKXP3"
+        email = env.str('ESKIZ_USER_EMAIL')
+        password = env.str('ESKIZ_USER_PASSWORD')
         eskiz = EskizSMS(email=email, password=password)
         eskiz.send_sms(mobile_phone=phone[1:], message=message_data, from_whom='4546', callback_url=None)
 
