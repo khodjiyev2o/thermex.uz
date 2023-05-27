16c5ce257cfccd9feedc5ef69b833270b45c7938
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-05-27 21:29:41 +0500

    fix: env file

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,19 +4,13 @@ from .base import *  # noqa
 DEBUG = True
 ALLOWED_HOSTS = ["18.228.10.255", "127.0.0.1", "api.san-neo.uz"]
 CORS_ORIGIN_ALLOW_ALL = True
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": env.str("DB_NAME"),
-#         "USER": env.str("DB_USER"),
-#         "PASSWORD": env.str("DB_PASSWORD"),
-#         "HOST": env.str("DB_HOST"),
-#         "PORT": env.str("DB_PORT"),
-#     },
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
+    },
 }
