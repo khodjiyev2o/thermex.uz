400212faec1a584fef068412b7e2e9547bf8556b
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-06-07 19:45:29 +0500

    feat: nginx conf

--- config/swagger.py
+++ config/swagger.py
@@ -3,8 +3,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-from .generator import BothHttpAndHttpsSchemaGenerator
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -15,7 +13,6 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    generator_class=BothHttpAndHttpsSchemaGenerator,
     permission_classes=(permissions.AllowAny,),
 )
 
